91b3dfc614202d2c2d40db8d78594004eeca946d
SadegH <sadegh.iraji@ymail.com>
2021-12-27 15:10:42 +0330

    add repositories

---
+++ src/main/java/com/zerofam/products/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.zerofam.products.repository;
+
+import com.zerofam.products.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
